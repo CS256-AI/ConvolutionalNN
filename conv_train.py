445af3ac51a584817a4033272e29eeae9221c52d
yeshbash <yeshbash@gmail.com>
2017-11-18 13:43:53 -0800

    arch changes

--- conv_train.py
+++ conv_train.py
@@ -5,14 +5,18 @@ op_classes = 10
 learning_rate = 0.01
 
 X = tf.placeholder()
+Y = tf.placeholder()
+
 labels = tf.placeholder()
 
 hidden_layers = list()
 logits = None
 
+
 def build_network(network_desc_file):
-    ## Building the network based on description file
+    # Building the network based on description file
     with open(network_desc_file) as f:
+        global X
         for line in f:
             params = line.split(" ")
             input_layer = X if len(hidden_layers) == 0 else input_layer = hidden_layers[-1]
@@ -29,15 +33,15 @@ def build_network(network_desc_file):
                 neurons = int(params[0])
                 input_layer = tf.contrib.layers.flatten(input_layer)
                 dense_layer = tf.layers.dense(inputs = input_layer, units=neurons, activation= tf.nn.relu)
-                logits = tf.layers.dense(inputs = dense_layer, units = op_classes)
+                logits = tf.layers.dense(inputs = dense_layer, units=1)
                 hidden_layers.append(dense_layer)
                 break
 
 
-## Defining loss function
-onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=op_classes)
-loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
+# Defining loss function
+loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=Y, logits=logits))
+tf.nn.si
 
-## Defining optimizer and training
+# Defining optimizer and training
 training = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
 
